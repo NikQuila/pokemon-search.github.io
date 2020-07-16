84b4fde341882e56047542bf3f27ae5d96843613
NikQuila <nicopirozzi1@gmail.com>
2020-07-15 22:15:27 -0400

    (dale)

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import "./card.styles.css";
+import Button from "react-bootstrap/Button";
+import CardBS from "react-bootstrap/Card";
 
 export const Card = (props) => (
-  <a href={`https://www.wikidex.net/wiki/${props.pokemon.name}`}>
-    <div className="card-container">
-      <img
-        alt="pokemon"
+  <CardBS bg="light" border="dark" text="primary" className="card-container">
+    <CardBS.ImgOverlay>
+      <CardBS.Img
+        variant="top"
         src={`https://img.pokemondb.net/sprites/omega-ruby-alpha-sapphire/dex/normal/${props.pokemon.name}.png`}
-      ></img>
-      <h2> {props.pokemon.name}</h2>
-    </div>
-  </a>
+      />
+    </CardBS.ImgOverlay>
+    <CardBS.Img src="https://static.vecteezy.com/system/resources/previews/000/625/844/non_2x/geometric-abstract-light-blue-background-for-website-wallpapers-bussines-templates-vector.jpg"></CardBS.Img>
+
+    <CardBS.Body>
+      <CardBS.Title>{props.pokemon.name}</CardBS.Title>
+      <Button variant="success">Check it</Button>
+    </CardBS.Body>
+  </CardBS>
 );
